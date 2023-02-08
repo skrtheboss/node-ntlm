c78b9d159b4d1c39f84e284eded9a2aa444aaa3a
skrtheboss <denisfrenademetz97@gmail.com>
2023-02-08 09:29:35 +0100

    fix: support complex www-authenticate headers

--- packages/axios/src/lib/axios.ts
+++ packages/axios/src/lib/axios.ts
@@ -1,7 +1,13 @@
 import * as http from 'node:http';
 import * as https from 'node:https';
 
-import { createType1Message, createType3Message, parseType2Message, Type3MessageOptions } from '@node-ntlm/core';
+import {
+    createType1Message,
+    createType3Message,
+    extractNtlmMessageFromAuthenticateHeader,
+    parseType2Message,
+    Type3MessageOptions,
+} from '@node-ntlm/core';
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import devnull from 'dev-null';
 
@@ -51,45 +57,42 @@ export function NtlmClient(credentials: NtlmCredentials, AxiosConfig?: AxiosRequ
         async (err: AxiosError) => {
             const error: AxiosResponse | undefined = err.response;
 
-            if (
-                error &&
-                error.status === 401 &&
-                error.headers['www-authenticate'] &&
-                error.headers['www-authenticate'].includes('NTLM')
-            ) {
-                // This length check is a hack because SharePoint is awkward and will
-                // include the Negotiate option when responding with the T2 message
-                // There is nore we could do to ensure we are processing correctly,
-                // but this is the easiest option for now
-                if (error.headers['www-authenticate'].length < 50) {
-                    const t1Msg = createType1Message(safeCredentials);
+            if (error && error.status === 401) {
+                const ntlmMessage = extractNtlmMessageFromAuthenticateHeader(error.headers['www-authenticate']);
 
-                    error.config.headers['Authorization'] = t1Msg;
-                } else {
-                    const t2Msg = parseType2Message(error.headers['www-authenticate']);
+                if (ntlmMessage) {
+                    // This length check is a hack because SharePoint is awkward and will
+                    // include the Negotiate option when responding with the T2 message
+                    // There is nore we could do to ensure we are processing correctly,
+                    // but this is the easiest option for now
+                    if (ntlmMessage.length < 50) {
+                        const t1Msg = createType1Message(safeCredentials);
 
-                    const t3Msg = createType3Message(t2Msg, safeCredentials);
+                        error.config.headers['Authorization'] = t1Msg;
+                    } else {
+                        const t3Msg = createType3Message(parseType2Message(ntlmMessage), safeCredentials);
 
-                    error.config.headers['X-retry'] = 'false';
-                    error.config.headers['Authorization'] = t3Msg;
-                }
-
-                if (error.config.responseType === 'stream') {
-                    const stream = err.response?.data as http.IncomingMessage | undefined;
-                    // Read Stream is holding HTTP connection open in our
-                    // TCP socket. Close stream to recycle back to the Agent.
-                    if (stream && !stream.readableEnded) {
-                        await new Promise<void>(resolve => {
-                            stream.pipe(devnull());
-                            stream.once('close', resolve);
-                        });
+                        error.config.headers['X-retry'] = 'false';
+                        error.config.headers['Authorization'] = t3Msg;
                     }
-                }
 
-                return client(error.config);
-            } else {
-                throw err;
+                    if (error.config.responseType === 'stream') {
+                        const stream = err.response?.data as http.IncomingMessage | undefined;
+                        // Read Stream is holding HTTP connection open in our
+                        // TCP socket. Close stream to recycle back to the Agent.
+                        if (stream && !stream.readableEnded) {
+                            await new Promise<void>(resolve => {
+                                stream.pipe(devnull());
+                                stream.once('close', resolve);
+                            });
+                        }
+                    }
+
+                    return client(error.config);
+                }
             }
+
+            throw err;
         }
     );
 
